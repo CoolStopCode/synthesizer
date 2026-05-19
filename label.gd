ba8c8eacf15f2ea510a4310e9875b4ac227a53e2
CoolStopCode <coolstopmail@gmail.com>
2026-05-18 22:11:42 -0500

    cleanup

--- label.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Key1"):
-		show()
-	if Input.is_action_just_released("Key1"):
-		hide()

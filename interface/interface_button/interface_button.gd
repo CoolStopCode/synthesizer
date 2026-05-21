8d451b4cd1f6fc3bba6b78c40ad34d8412642f7c
CoolStopCode <coolstopmail@gmail.com>
2026-05-20 21:18:47 -0500

    buttons

--- interface/interface_button/interface_button.gd
+++ interface/interface_button/interface_button.gd
@@ -1,12 +1,74 @@
+@tool
 class_name InterfaceButton
 extends Control
 
+enum ButtonColor {
+	BLUE,
+	RED,
+	WHITE,
+	YELLOW
+}
 
-# Called when the node enters the scene tree for the first time.
+enum ButtonState {
+	NORMAL,
+	PRESSED,
+	HOVERED
+}
+
+var textures := {
+	ButtonColor.BLUE: {
+		ButtonState.NORMAL: preload("res://interface/interface_button/apollo4/blue.png"),
+		ButtonState.PRESSED: preload("res://interface/interface_button/apollo4/blue_pressed.png"),
+		ButtonState.HOVERED: preload("res://interface/interface_button/apollo4/blue_hovered.png")
+	},
+
+	ButtonColor.RED: {
+		ButtonState.NORMAL: preload("res://interface/interface_button/apollo4/red.png"),
+		ButtonState.PRESSED: preload("res://interface/interface_button/apollo4/red_pressed.png"),
+		ButtonState.HOVERED: preload("res://interface/interface_button/apollo4/red_hovered.png")
+	},
+
+	ButtonColor.WHITE: {
+		ButtonState.NORMAL: preload("res://interface/interface_button/apollo4/white.png"),
+		ButtonState.PRESSED: preload("res://interface/interface_button/apollo4/white_pressed.png"),
+		ButtonState.HOVERED: preload("res://interface/interface_button/apollo4/white_hovered.png")
+	},
+
+	ButtonColor.YELLOW: {
+		ButtonState.NORMAL: preload("res://interface/interface_button/apollo4/yellow.png"),
+		ButtonState.PRESSED: preload("res://interface/interface_button/apollo4/yellow_pressed.png"),
+		ButtonState.HOVERED: preload("res://interface/interface_button/apollo4/yellow_hovered.png")
+	}
+}
+
+@export var button_color : ButtonColor:
+	set(value):
+		button_color = value
+		_update_texture()
+
+@export var pressed : bool = false
+@export var hovered : bool = false
+@export var texture_node : TextureRect
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_update_texture()
+
+func _update_texture() -> void:
+	var state : ButtonState
+	if pressed:
+		state = ButtonState.PRESSED
+	elif hovered:
+		state = ButtonState.HOVERED
+	else:
+		state = ButtonState.NORMAL
+	
+	print(state)
+	texture_node.texture = textures[button_color][state]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			pressed = event.pressed
+			_update_texture()

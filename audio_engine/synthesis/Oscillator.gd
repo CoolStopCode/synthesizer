1d847ecd32c86e5e0883e20e31f622ef427ca554
CoolStopCode <coolstopmail@gmail.com>
2026-06-05 14:11:47 -0500

    custom waveforms

--- audio_engine/synthesis/Oscillator.gd
+++ audio_engine/synthesis/Oscillator.gd
@@ -1,12 +1,12 @@
 class_name Oscillator
 
-var waveform : Waveform.Enum
+var waveform : Waveform
 var phase : float = 0.0
 var frequency : float
 var gain : float
 
 func _init(
-	_waveform : Waveform.Enum = Waveform.Enum.SAW, 
+	_waveform : Waveform = Waveform.new(),
 	_frequency : float = 440.0,
 	_gain : float = 1.0
 ) -> void:
@@ -15,29 +15,10 @@ func _init(
 	gain = _gain
 
 func process(delta : float) -> float:
-	var value := evaluate(phase)
+	var value := waveform.evaluate(phase)
 
 	# advance phase
 	phase += frequency * delta
 	phase = fmod(phase, 1.0)
 	
 	return value * gain
-
-
-func evaluate(p: float) -> float:
-	match waveform:
-		Waveform.Enum.SINE:
-			return sin(p * TAU)
-
-		Waveform.Enum.SQUARE:
-			return 1.0 if p < 0.5 else -1.0
-
-		Waveform.Enum.SAW:
-			return (p * 2.0) - 1.0
-
-		Waveform.Enum.TRIANGLE:
-			return 1.0 - 4.0 * abs(p - 0.5)
-		
-		_:
-			
-			return 0.0
--- audio_engine/synthesis/OscillatorLayer.gd
+++ audio_engine/synthesis/OscillatorLayer.gd
@@ -5,5 +5,5 @@ extends Resource
 @export var chord_note : int = 0
 @export var octave_shift : int = 0
 @export var semitone_shift : int = 0
-@export var waveform : Waveform.Enum = Waveform.Enum.SINE
+@export var waveform : Waveform = Waveform.new()
 @export var gain : float = 0.5

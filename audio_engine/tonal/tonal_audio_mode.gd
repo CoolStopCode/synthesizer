8c96e6afa7bf5f82dad2a6362bebb278d29d558e
CoolStopCode <coolstopmail@gmail.com>
2026-07-13 21:30:49 -0500

    fonts

--- audio_engine/tonal/tonal_audio_mode.gd
+++ audio_engine/tonal/tonal_audio_mode.gd
@@ -22,11 +22,12 @@ func build_chord(index: int) -> Chord:
 	return bend_binding.build_chord(scale, index, bend)
 
 func build() -> void:
-	scale = key.build_scale()
+	scale= key.build_scale()
 	
 	var chords : Array[Chord]
 	for i in range(7):
 		var chord : Chord = Chord.new()
+		print(scale.root_note(i).get_semitone(0).to_string_name())
 		chords.append(chord)
 	
-	#build()
+	

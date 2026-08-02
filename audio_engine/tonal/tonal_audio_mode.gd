162f1506c036f0e733151e5f391073277a349f04
CoolStopCode <coolstopmail@gmail.com>
2026-08-01 21:42:11 -0500

    android stuff

--- audio_engine/tonal/tonal_audio_mode.gd
+++ audio_engine/tonal/tonal_audio_mode.gd
@@ -3,13 +3,13 @@
 extends AudioMode
 
 enum Allocation {
-	POLY, # Any polyvoices can be active and unmuted
-	MONO, # Only one polyvoice can be active, but any can be unmuted
-	LEGATO, # Only one polyvoice can be active and unmuted
+	POLY, ## Any polyvoices can be active and unmuted
+	MONO, ## Only one polyvoice can be active, but any can be unmuted
+	LEGATO, ## Only one polyvoice can be active and unmuted
 }
 
 @export var polyvoice_count: int = 7
-@export var voice_count: int = 5 ## Voices per polyvoice
+@export var voice_count: int = 5 ## Voices per polyvoiceuiop
 @export var allocation: Allocation
 @export var fade_duration : float = 0.05 ## Used for legato allocation, prevents clipping
 @export var chord_bend_duration : float = 0.1 ## Linear fade time for bending an active polyvoice
@@ -23,9 +23,3 @@ func build_chord(index: int) -> Chord:
 
 func build() -> void:
 	scale = key.build_scale()
-	
-	var chords : Array[Chord]
-	for i in range(polyvoice_count):
-		var chord : Chord = Chord.new()
-		#print(scale.root_note(i).get_semitone(0).to_string_name())
-		chords.append(chord)
